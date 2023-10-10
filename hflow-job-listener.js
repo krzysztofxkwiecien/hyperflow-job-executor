5a6516fdebcdd8874e49d293fc6528c0da8c5587
Krzysztof Kwiecień <krzysztofxkwiecien@gmail.com>
2023-10-10 18:56:59 +0200

    http listener

--- hflow-job-listener.js
+++ hflow-job-listener.js
@@ -1,99 +1,39 @@
 #!/usr/bin/env node
 
-const amqp = require('amqplib/callback_api'),
-    redis = require('redis'),
+const express = require('express');
+const app = express();
+const redis = require('redis'),
     rcl = redis.createClient(process.env.REDIS_URL),
-    uuid = require('uuid'),
     handleJob = require('./handler').handleJob;
 
-const queue = process.env.QUEUE_NAME;
-const CONSUMER_TAG = uuid.v4();
+app.use(express.json())
 
-let connection_handler = null
-let channel_handler = null
-let msg_processing = false
-let consumer_created = false
-let consumer_cancelled = false
+app.post("/", (req, res) => {
 
-process.on('SIGTERM', async () => {
-    console.log("SIGTERM received. Closing process")
-    if (channel_handler !== null && consumer_created) {
-        await channel_handler.cancel(CONSUMER_TAG);
-    }
-    consumer_cancelled = true
+    console.log("Received task with taskId [%s]", req.body.taskId);
 
-    if (msg_processing === false) {
-        setTimeout(closeConnections, 5000);
-    }
+    executeJob(req.body).then((value) => {
+        console.log("Handling done")
+        res.sendStatus(200);
+    }).catch(function () {
+        console.error("Handling error")
+        res.sendStatus(500);
+    });
 })
 
-async function executeTask(tasks) {
-    for (let idx = 0; idx < tasks.length; idx++) {
-        let jobExitCode = await handleJob(tasks[idx].id, rcl, tasks[idx].message);
-        console.log("Task", tasks[idx], "job exit code:", jobExitCode);
-    }
+async function executeJob(jobMessage) {
+    let jobExitCode = await handleJob(jobMessage.taskId, rcl, jobMessage);
+    console.log("Job", jobMessage.taskId, "job exit code:", jobExitCode);
 }
 
-async function onMessage(channel, msg) {
-    console.log(" [x] Received %s", msg.content.toString());
-    msg_processing = true
-    executeTask(JSON.parse(msg.content).tasks).then((value) => {
-        console.log("Message completed")
-        channel.ack(msg)
-        msg_processing = false
-    }).catch(function () {
-        console.error("Message processing error")
-        channel.nack(msg);
-        msg_processing = false
-    }).finally(function () {
-        if (consumer_cancelled === true) {
-            setTimeout(closeConnections, 5000);
-        }
-    });
-}
+app.listen(8080, () => {
+    console.log(`Server listening on port 8080`);
+});
 
-function onChannelCreated(error, channel) {
-    if (error) {
-        throw error;
-    }
-    channel_handler = channel
-    const consumerOptions = {noAck: false, consumerTag: CONSUMER_TAG}
-    const queueOptions = {durable: false, expires: 6000000}
-    const prefetch = parseInt(process.env['RABBIT_PREFETCH_SIZE']) || 1
+rcl.on('ready', () => {
+    console.log('Redis ready');
+});
 
-    channel.prefetch(prefetch);
-    channel.assertQueue(queue, queueOptions);
-
-    console.log(" [*] Waiting for messages in queue: %s", queue);
-    console.log("Consumer tag: " + CONSUMER_TAG);
-    channel.consume(queue, (msg) => onMessage(channel, msg), consumerOptions);
-    consumer_created = true
-}
-
-function onConnectionCreated(error, connection) {
-    if (error) {
-        throw error;
-    }
-    connection_handler = connection
-    connection.createChannel(onChannelCreated);
-}
-
-async function closeConnections() {
-    console.log("Terminate listener invoked")
-    if (channel_handler !== null) {
-        await channel_handler.close()
-        console.log("RabbitMQ channel closed")
-    }
-    if (connection_handler !== null) {
-        await connection_handler.close()
-        console.log("RabbitMQ connection closed")
-    }
-    if (rcl !== null) {
-        await rcl.quit()
-        console.log("Redis connection closed")
-    }
-    console.log("Terminate listener processed")
-    process.exit(0)
-}
-
-amqp.connect(`amqp://${process.env.RABBIT_HOSTNAME}`, onConnectionCreated);
+rcl.on('error', (err) => {
+    console.error('Redis error:', err);
+});
